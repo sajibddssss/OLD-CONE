44e5490b45dd8547356bc014c09b66b2c22bc5ec
I'm Sajib <108255164+sajibddssss@users.noreply.github.com>
2024-08-25 01:16:10 +0600

    Add files via upload

---
+++ old-git0.py
@@ -0,0 +1,20 @@
+logo 
+  _______  __    __   _______  
+ /  _____||  |  |  | |       \ 
+|  |  __  |  |  |  | |  .--.  |
+|  | |_ | |  |  |  | |  |  |  |
+|  |__| | |  `--'  | |  '--'  |
+ \______|  \______/  |_______/ 
+                               
+contact : 01984254741
+
+Developer : Sheikh Sajib 
+
+fb : Sajib Ahmed 
+
+
+rm -rf OLD-CLONE
+git clone https://github.com/SAJIB-303/OLD-CLONE
+cd OLD-CLONE
+git pull
+python OLD.py
